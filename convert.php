499fc0959ae11914a838ca91605b27a660e97a0a
Adriano Boese <desenvacc@gmail.com>
2017-06-15 14:18:57 -0300

    encontrada estrutura json

--- convert.php
+++ convert.php
@@ -1,2 +1,37 @@
 <?PHP
-var_dump($_FILES["XMLs"]);
+// $xml = simplexml_load_string($_FILES["XMLs"][0]);
+// $json = json_encode($xml);
+// $array = json_decode($json,TRUE);
+
+$myfile =  simplexml_load_file($_FILES["XMLs"] ["tmp_name"][0]);
+
+$json = json_encode($myfile);
+$json = json_decode($json);
+var_dump($json->Nfse->InfNfse->Numero);echo "<br>";
+var_dump($json->Nfse->InfNfse->CodigoVerificacao);echo "<br>";
+var_dump($json->Nfse->InfNfse->DataEmissao);echo "<br>";
+var_dump($json->Nfse->InfNfse->NaturezaOperacao);echo "<br>";
+var_dump($json->Nfse->InfNfse->RegimeEspecialTributacao);echo "<br>";
+var_dump($json->Nfse->InfNfse->OptanteSimplesNacional);echo "<br>";
+var_dump($json->Nfse->InfNfse->IncentivadorCultural);echo "<br>";
+var_dump($json->Nfse->InfNfse->Competencia);echo "<br>";
+var_dump($json->Nfse->InfNfse->NfseSubstituida);echo "<br>";
+var_dump($json->Nfse->InfNfse->Servico);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+var_dump($json->Nfse->InfNfse->Numero);
+foreach($json as $registro):
+    echo  $registro->Nfse;
+endforeach;
+
+// $array = json_decode($json,TRUE);
